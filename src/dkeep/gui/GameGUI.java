60239bd1a07df21b1a23389b6eead73086adcf85
pfranca <pedro.franca.1994@gmail.com>
2018-03-30 15:30:41 +0100

    kinda working, looks good at least :) cannot play a new game after game over :(

--- src/dkeep/gui/GameGUI.java
+++ src/dkeep/gui/GameGUI.java
@@ -32,6 +32,16 @@ public class GameGUI {
 
 	private JFrame frame;
 	private static Game game;
+	JButton btnExit;
+	JTextArea gameText;
+	JLabel lblGuardsPersonality;
+	JComboBox comboBox;
+	JTextArea gameMessage;
+	JButton btnUp;
+	JButton btnDown;
+	JButton btnLeft;
+	JButton btnRight;
+	JButton btnNewGame;
 
 	/**
 	 * Launch the application.
@@ -66,7 +76,7 @@ public class GameGUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 				
-		JButton btnExit = new JButton("Exit");
+		btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
@@ -75,41 +85,50 @@ public class GameGUI {
 		btnExit.setBounds(455, 276, 89, 23);
 		frame.getContentPane().add(btnExit);
 		
-		JTextArea gameText = new JTextArea();
+		gameText = new JTextArea();
 		gameText.setBounds(22, 46, 305, 224);
-		gameText.setFont(new Font("Courier New", Font.PLAIN,15));
+		gameText.setFont(new Font(Font.MONOSPACED/*"Courier New"*/, Font.PLAIN,15));
 		frame.getContentPane().add(gameText);
 		
-		JLabel lblGuardsPersonality = new JLabel("Guard Personality");
+		lblGuardsPersonality = new JLabel("Guard Personality");
 		lblGuardsPersonality.setBounds(36, 27, 128, 14);
 		frame.getContentPane().add(lblGuardsPersonality);
 		
-		JComboBox comboBox = new JComboBox();
+		comboBox = new JComboBox();
 		comboBox.setBounds(182, 24, 106, 20);
 		frame.getContentPane().add(comboBox);
 		comboBox.addItem("Rookie");
 		comboBox.addItem("Drunken");
 		comboBox.addItem("Suspicious");
 		
-		JTextArea gameMessage = new JTextArea();
+		gameMessage = new JTextArea();
 		gameMessage.setBounds(22, 284, 234, 15);
 		gameMessage.setText("You can start a new game!");
 		frame.getContentPane().add(gameMessage);
 		
-		JButton btnUp = new JButton("Up");
+		btnUp = new JButton("Up");
 		btnUp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				game.moveHero("w");
-	            game.moveGuard(game.getGuard());
-	            gameMessage.setText("you moved up");
-	            gameText.setText(null);
-	            for(int i = 0; i<game.getMap().getMap().length; i++){
-			          for(int j = 0; j<game.getMap().getMap().length; j++){
-			             gameText.append((game.getMap().getMap()[i][j])+"");
-			          }
-			          gameText.append("\n");
-			    }
+				if(game.isGameOver()) {
+					gameMessage.setText("Game Over");
+					btnUp.setEnabled(false);
+					btnDown.setEnabled(false);
+					btnLeft.setEnabled(false);
+					btnRight.setEnabled(false);
+					
+				}else {
+					game.moveHero("w");
+		            game.moveGuard(game.getGuard());
+		            gameMessage.setText("you moved up");
+		            gameText.setText(null);
+		            for(int i = 0; i<game.getMap().getMap().length; i++){
+				          for(int j = 0; j<game.getMap().getMap().length; j++){
+				             gameText.append((game.getMap().getMap()[i][j])+"");
+				          }
+				          gameText.append("\n");
+				    }
+				}
 	            
 			}
 		});
@@ -117,20 +136,29 @@ public class GameGUI {
 		btnUp.setEnabled(false);
 		frame.getContentPane().add(btnUp);
 		
-		JButton btnDown = new JButton("Down");
+		btnDown = new JButton("Down");
 		btnDown.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				game.moveHero("s");
-	            game.moveGuard(game.getGuard());
-	            gameMessage.setText("you moved down");
-	            gameText.setText(null);
-	            for(int i = 0; i<game.getMap().getMap().length; i++){
-			          for(int j = 0; j<game.getMap().getMap().length; j++){
-			             gameText.append((game.getMap().getMap()[i][j])+"");
-			          }
-			          gameText.append("\n");
-			    }
+				if(game.isGameOver()) {
+					gameMessage.setText("Game Over");
+					btnUp.setEnabled(false);
+					btnDown.setEnabled(false);
+					btnLeft.setEnabled(false);
+					btnRight.setEnabled(false);
+					
+				}else {
+					game.moveHero("s");
+		            game.moveGuard(game.getGuard());
+		            gameMessage.setText("you moved down");
+		            gameText.setText(null);
+		            for(int i = 0; i<game.getMap().getMap().length; i++){
+				          for(int j = 0; j<game.getMap().getMap().length; j++){
+				             gameText.append((game.getMap().getMap()[i][j])+"");
+				          }
+				          gameText.append("\n");
+				    }
+				}
 	            
 			}
 		});
@@ -138,20 +166,29 @@ public class GameGUI {
 		btnDown.setEnabled(false);
 		frame.getContentPane().add(btnDown);
 		
-		JButton btnLeft = new JButton("Left");
+		btnLeft = new JButton("Left");
 		btnLeft.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				game.moveHero("a");
-	            game.moveGuard(game.getGuard());
-	            gameMessage.setText("you moved left");
-	            gameText.setText(null);
-	            for(int i = 0; i<game.getMap().getMap().length; i++){
-			          for(int j = 0; j<game.getMap().getMap().length; j++){
-			             gameText.append((game.getMap().getMap()[i][j])+"");
-			          }
-			          gameText.append("\n");
-			    }
+				if(game.isGameOver()) {
+					gameMessage.setText("Game Over");
+					btnUp.setEnabled(false);
+					btnDown.setEnabled(false);
+					btnLeft.setEnabled(false);
+					btnRight.setEnabled(false);
+					
+				}else {
+					game.moveHero("a");
+		            game.moveGuard(game.getGuard());
+		            gameMessage.setText("you moved left");
+		            gameText.setText(null);
+		            for(int i = 0; i<game.getMap().getMap().length; i++){
+				          for(int j = 0; j<game.getMap().getMap().length; j++){
+				             gameText.append((game.getMap().getMap()[i][j])+"");
+				          }
+				          gameText.append("\n");
+				    }
+				}
 	            
 			}
 		});
@@ -159,20 +196,29 @@ public class GameGUI {
 		btnLeft.setEnabled(false);
 		frame.getContentPane().add(btnLeft);
 		
-		JButton btnRight = new JButton("Right");
+		btnRight = new JButton("Right");
 		btnRight.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				game.moveHero("d");
-	            game.moveGuard(game.getGuard());
-	            gameMessage.setText("you moved right");
-	            gameText.setText(null);
-	            for(int i = 0; i<game.getMap().getMap().length; i++){
-			          for(int j = 0; j<game.getMap().getMap().length; j++){
-			             gameText.append((game.getMap().getMap()[i][j])+"");
-			          }
-			          gameText.append("\n");
-			    }
+				if(game.isGameOver()) {
+					gameMessage.setText("Game Over");
+					btnUp.setEnabled(false);
+					btnDown.setEnabled(false);
+					btnLeft.setEnabled(false);
+					btnRight.setEnabled(false);
+					
+				}else {
+					game.moveHero("d");
+		            game.moveGuard(game.getGuard());
+		            gameMessage.setText("you moved right");
+		            gameText.setText(null);
+		            for(int i = 0; i<game.getMap().getMap().length; i++){
+				          for(int j = 0; j<game.getMap().getMap().length; j++){
+				             gameText.append((game.getMap().getMap()[i][j])+"");
+				          }
+				          gameText.append("\n");
+				    }
+				}
 	            
 			}
 		});
@@ -180,22 +226,25 @@ public class GameGUI {
 		btnRight.setEnabled(false);
 		frame.getContentPane().add(btnRight);
 		
-		JButton btnNewGame = new JButton("Start New Game");
+		btnNewGame = new JButton("Start New Game");
 		btnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				GameGUI.game= new Game(new GameMap(level1));
-				btnUp.setEnabled(true);
-				btnDown.setEnabled(true);
-				btnLeft.setEnabled(true);
-				btnRight.setEnabled(true);
-				gameMessage.setText("You can play now");
-				for(int i = 0; i<game.getMap().getMap().length; i++){
-			          for(int j = 0; j<game.getMap().getMap().length; j++){
-			             gameText.append((game.getMap().getMap()[i][j])+"");
-			          }
-			          gameText.append("\n");
-			    }
-			}
+				gameText.setText(null);
+				
+					GameGUI.game= new Game(new GameMap(level1));
+					btnUp.setEnabled(true);
+					btnDown.setEnabled(true);
+					btnLeft.setEnabled(true);
+					btnRight.setEnabled(true);
+					gameMessage.setText("You can play now");
+					for(int i = 0; i<game.getMap().getMap().length; i++){
+				          for(int j = 0; j<game.getMap().getMap().length; j++){
+				             gameText.append((game.getMap().getMap()[i][j])+"");
+				          }
+				          gameText.append("\n");
+				    }
+				}
+			
 		});
 		btnNewGame.setBounds(363, 42, 165, 23);
 		frame.getContentPane().add(btnNewGame);
