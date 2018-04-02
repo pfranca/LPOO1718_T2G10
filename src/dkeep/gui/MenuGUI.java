397f73ac9058276f795c746f2f49c52d4e68709f
pfranca <pedro.franca.1994@gmail.com>
2018-04-02 22:18:57 +0100

    menu ui

--- src/dkeep/gui/MenuGUI.java
+++ src/dkeep/gui/MenuGUI.java
@@ -1,17 +1,21 @@
 package dkeep.gui;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 
 public class MenuGUI extends JPanel{
 	
-	private JButton btnNewGame;
 	private JFrame gameFrame = GameGUI.gameFrame;
 	
 	public MenuGUI() {
 		super();
 		
-		//setBtnNewGame();
-		//setBtnExit();
+		setBtnNewGame();
+		setBtnExit();
 		//setBtnMapMaker();
 		
 		setImage();
@@ -20,9 +24,40 @@ public class MenuGUI extends JPanel{
 	private void setImage() {
 		JPanel menuImgPanel = new JPanel();
 		menuImgPanel.setBounds(0, 0, 1000, 1000);
-		ImageIcon menuImg = new ImageIcon(this.getClass().getResource("res/menu.png"));
+		ImageIcon menuImg = new ImageIcon(this.getClass().getResource("res/menu2.png"));
 		menuImgPanel.add(new JLabel(menuImg));
 		add(menuImgPanel);
+	}
+	
+private void setBtnExit() {
+		
+		JButton btnExit = new JButton("");
+		btnExit.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		btnExit.setBackground(Color.BLACK);
+		btnExit.setIcon(new ImageIcon(MenuGUI.class.getResource("res/btnExit3.jpg")));
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnExit.setBounds(650, 860, 275, 65);
+		add(btnExit);	
+	}
+
+public void setBtnNewGame(){
+	
+		JButton btnNewGame  = new JButton("");
+		btnNewGame.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		btnNewGame.setBackground(Color.BLACK);
+		btnNewGame.setIcon(new ImageIcon(MenuGUI.class.getResource("res/btnNewGame3.jpg")));
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+					
+			}
+		});
+		btnNewGame.setBounds(600, 750, 320, 86);
+		add(btnNewGame);
+		
 	}
 
 }
