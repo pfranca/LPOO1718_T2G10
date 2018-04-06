95a3dd9537d2ddb015c05ea040af0929c4e57c34
pfranca <pedro.franca.1994@gmail.com>
2018-04-06 20:28:18 +0100

    logic documentation finnished

--- src/dkeep/logic/Key.java
+++ src/dkeep/logic/Key.java
@@ -1,7 +1,19 @@
 package dkeep.logic;
 
+/**  
+* Key.java - Represents the Key element of the game
+* @author  Patricia Janeiro
+* @author  Pedro França
+* @version 1.0 
+* @see Element.java
+*/
 public class Key extends Element {
 	
+	/**  
+	*Key class constructor
+	*
+	*@param map An instance of GameMap containing a map with an Key element
+	*/
 	public Key(GameMap map) {
 		super(map, 'k');
 	}
--- src/dkeep/logic/Ogre.java
+++ src/dkeep/logic/Ogre.java
@@ -1,17 +1,37 @@
 package dkeep.logic;
 
+/**  
+* Key.java - Represents the Key element of the game
+* @author  Patricia Janeiro
+* @author  Pedro França
+* @version 1.0 
+* @see Element.java
+*/
 public class Ogre extends Element{
 	
 	private CellPosition club;
 	
+	/**  
+	*Ogre class constructor
+	*
+	*@param map An instance of GameMap containing a map with an Ogre element
+	*/
 	public Ogre(GameMap map) {
 		super(map, 'O');
 	}
 
+	/**  
+	* @return a CellPosition object coitaing the position of the ogre's club
+	*/
 	public CellPosition getClub() {
 		return club;
 	}
 	
+	/**  
+	* @param x x value of the club position
+	* @param x x value of the club position
+	* @return Nothing.
+	*/
 	public void setClub(int x, int y) {
 		club.set(x, y);
 	}
